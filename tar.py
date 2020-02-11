243eb6c084a6418dc7d425a74e5d11f1fda5e715
RoppeltF <31130300+RoppeltF@users.noreply.github.com>
2020-02-10 21:02:54 -0500

    Add files via upload
    tar.py - Go through all lists on the page limited to 5 [0:4]pages for test porpuses

--- tar.py
+++ tar.py
@@ -6,49 +6,69 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as expectedCondition
 
+
 import re
 import time
+import argparse
 
-from dataB.pysql import insertFila,getFila,connect
+
+#from dataB.pysql import insertFila,getFila,connect
 
 #Pavadinimas - Title
 #Institucijos suteiktas nr. - Authority issued no.
 #Priėmimo data - Date of Admission
 #Įsigaliojimo data - Entry into force
 
+
 global browser
 global BASE_URL
 global links
 global nomes_lista
 global datas
+global WT
+
+#https://levelup.gitconnected.com/the-easy-guide-to-python-command-line-arguments-96b4607baea1
+
+parser = argparse.ArgumentParser(description='Robo de coleta de novos lituanos')
+parser.add_argument("--headless", action="store_const", const=True,help="Headless mode - não abrira o navegador e rodara em 2ndo plano")
+parser.add_argument("-wt", action="store_const", const='10', help="Wait time")
+#parser.add_argument("--name", required=True, type=str, help="Your name")
+
+args = parser.parse_args()
+#a = args.a
+hdless = args.headless
+WT = args.wt
+
 
 nomes_lista = []; datas = []; links = [];
 
 BASE_URL = "https://www.e-tar.lt"
 
-headless = Options()
-#headless.add_argument("--headless")
-headless.headless = True
-
-#browser = webdriver.Firefox(options=headless)
-browser = webdriver.Firefox()
+if hdless == True: 
+    headless = Options()
+    headless.add_argument("--headless")
+    headless.headless = True
+    browser = webdriver.Firefox(options=headless)
+else:    
+    browser = webdriver.Firefox()
 
 def tarDefault():
+    global WT
     browser.get("https://www.e-tar.lt/acc/index.html")
     assert "TAR" in browser.title
     #Find the search box - Localiza a caixa de pesquisa
     elem = browser.find_element_by_name("page-index:j_id_v:page-index-search")
 
-        #Search query - texto da pesquisa
+    #Search query - texto da pesquisa
     elem.send_keys("pilietybę Brazilijoje" + Keys.RETURN)
 
-        # selenium sleep driverName + time or it waits untill  expected condition is met - Tempo de espera do Selenium Nome do driver+tempo ou espera a condicao ser atendida
-    WebDriverWait(browser, 5).until(expectedCondition.presence_of_element_located((By. XPATH, "//div[@class='table-content-inner']")))
+    # selenium sleep driverName + time or it waits untill  expected condition is met - Tempo de espera do Selenium Nome do driver+tempo ou espera a condicao ser atendida
+    WebDriverWait(browser, 10).until(expectedCondition.presence_of_element_located((By. XPATH, "//div[@class='table-content-inner']")))
 
     #printa NR + data no terminal da pagina mais recente  ex.Lnk + 1V-860 + data YYYY-MM-DD
     # link | Issued No. | Date
 
-    WebDriverWait(browser,7 ).until(expectedCondition.presence_of_element_located((By. XPATH, "//div[@class = 'table-row']")))
+    WebDriverWait(browser, 10).until(expectedCondition.presence_of_element_located((By. XPATH, "//div[@class = 'table-row']")))
 
     for a in browser.find_elements_by_xpath("//div[@class = 'table-row']"):
         datas.append( re.search(r'[\d][\d][\d][\d][-][\d][\d][-][\d][\d]',a.text).group(0) )
@@ -62,6 +82,7 @@ def tarDefault():
         links.append( re.findall("[^=]+[0-9]+[a-z0-9]*",lnk.get_attribute('href') ) )
 
 #    print(links)
+    print("")
 
     #Find All links (* element(S) - element get only the first occurrence)
     #browser.find_element_by_xpath('.//a').click()
@@ -74,40 +95,38 @@ def tarDefault():
 #Get all brazilians names from the list | coleta os nomes de brasileiros da lista
 def getNomes(links,data):
         global BASE_URL
+        for url in links[0:4]:
+#        browser.get(BASE_URL + "/rs/legalact/" + str(links[0][0]))
+            browser.get( BASE_URL + "/rs/legalact/" + url[0] )
 
-#    for url in links:
-        browser.get(BASE_URL + "/rs/legalact/" + str(links[0][0]))
-        print(links[0][0])
-#        browser.get(BASE_URL + "/rs/legalact/" + url[0])
-#        print(url)
+            text=[];listaBR=[];nomes_tmp=[]
+           
+            for elem in browser.find_elements(By.CLASS_NAME,"MsoNormal"):
+                text.append( elem.text )
+            i = 0
+            while i < len(text):
+                if re.search(r'Brazil',text[i]):
+                    listaBR.append( text[i].split(',') )
+                    time.sleep(1)
+                i += 1
+            i=0
 
-        texto=[];listaBR=[];nomes_tmp=[]
+            while i < len(listaBR):
+               nomes_tmp.append( listaBR[i][0] )
+#               print( nomes_tmp )
+               i += 1
 
-        for elem in browser.find_elements(By.CLASS_NAME,"MsoNormal"):
-            texto.append( elem.text )
-        i = 0
-        while i < len(texto):
-            if re.search(r'Brazil',texto[i]):
-                listaBR.append( texto[i].split(',') )
-                time.sleep(1)
-            i += 1
-        i=0
+            nomes_lista.append(nomes_tmp)
 
-        while i < len(listaBR):
-           nomes_tmp.append( listaBR[i][0] )
-#           print( nomes_tmp )
-           i += 1
-        nomes_lista.append(nomes_tmp)
-
-        print("Nomes Lista ")
-        print (*nomes_lista[0],sep='\n')
-
+#       print("Nomes Lista ")
+#       print (*nomes_lista[0],sep='\n')
+#       print("")
 
         return nomes_lista
 
 
-#def insertDados(link,datas):
-def insertDados():
+def insertDados(nomes,link,datas):
+#def insertDados():
     link = []
     nomes = []
 
@@ -137,11 +156,12 @@ def insertDados():
     print("")
     print(val)
 
-#    dBase.executemany(sql, val)
-#    mydb.commit()
-#    print(dBase.rowcount, "was inserted.")
-#    dBaseClose()
+    dBase.executemany(sql, val)
+    mydb.commit()
+    print(dBase.rowcount, "was inserted.")
+    dBaseClose()
     return 0
+
 
 def checkDados():
     dBase.execute( 'SELECT link FROM listas ORDER BY id DESC LIMIT 1;' )
@@ -158,162 +178,57 @@ links = default_result[1]
 datas = default_result[0]
 
 nomes_lista = getNomes(links,datas)
+nomes = nomes_lista
 
 
-print("")
-print("")
-
-#for x in range(len(nomes_lista[0])):
-#    print(nomes_lista[x])
-
-
-
-print("")
-print("")
 print("Links")
-print (links)
-
-print("")
+print(links)
+print(len(links))
 print("")
 print("Datas")
-print (datas)
+print(datas)
+print(len(datas))
+print("")
+print("Nomes")
+print(nomes)
+print(len(nomes))
+print("")
+
+
+x=0 
+
+DLN = []
+
+while( x < len(datas) ):
+    DLN.setdefault(datas[x], []).append({links[x]:nomes[x]})
+
+#    dateLink.setdefault(datas[x],[]).append(nomes[0])
+    x += 1
+
+data, url = DLN.items()[0]             
+
+print("Dict datas - link : nomes")
+#print(dateLinkNomes)
+print("The 1st key of dictionary is : " + str(data)) 
+print("The 1st value of dictionary is : " + str(url)) 
+
+
+#print("Data da(s) ultima(s) lista(s) " + datas[0] + "total de BR's:  " + str(len(nomes)) )
+
+#print("")
+#print("Nome(s)")
+
+#for x in nomes:
+#    print(x)
+
+
+#print("")
+#print("Links")
+#print(BASE_URL + "/rs/legalact/" + str(links[0][0]))
+
 
 
 #prev_result = checkDados()
 
-#Whats()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-########### WHATS APP  ##############
-
-#
-## Replace below path with the absolute path
-## to chromedriver in your computer
-#
-#
-##options = Options()
-##options.add_argument("user-data-dir=profile/")
-##options.add_argument("")
-#
-#
-##driver = webdriver.Chrome('./chromedriver')
-##driver = webdriver.Chrome('/home/tux/DEV/python/whatspython/whatsappmessagepython/chromedriver', options=options)
-#
-#
-#driver = webdriver.Firefox()
-#
-#driver.get("https://web.whatsapp.com/")
-
-#load Cookies
-#for cookie in pickle.load(open("whats.pkl", "rb")):
-#    driver.add_cookie(cookie)
-
-
-#wait = WebDriverWait(driver, 800)
-#
-# Replace 'Friend's Name' with the name of your friend
-# or the name of a group
-#target = '"Lietuva"'
-#
-## Replace the below string with your own message
-#string = sys.argv[1]
-#string = msg
-#string = "teste"
-#
-#x_arg = '//span[contains(@title,' + target + ')]'
-#group_title = wait.until(expectedCondition.presence_of_element_located((By.XPATH, x_arg)))
-#group_title.click()
-#
-#
-#message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
-#
-#message.send_keys(string)
-#
-#sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button')[0]
-#sendbutton.click()
-
-
-#pickle.dump( browser.get_cookies(), open("whats.pkl","wb") )
-
-#cookies = []
-
-
-
-
-#
-#
-##Salvar cookies
-## Go to the correct domain
-#
-## Now set the cookie. Here's one for the entire domain
-## the cookie name here is 'key' and its value is 'value'
-#driver.add_cookie({'name':'key', 'value':'value', 'path':'profile'})
-#
-## additional keys that can be passed in are:
-## 'domain' -> String,
-## 'secure' -> Boolean,
-## 'expiry' -> Milliseconds since the Epoch it should expire.
-#
-## And now output all the available cookies for the current URL
-#for cookie in driver.get_cookies():
-#    print ( "%s -> %s" % (cookie['name'], cookie['value']) )
-#
-## You can delete cookies in 2 ways
-## By name
-##driver.delete_cookie("CookieName")
-# Or all of them
-#driver.delete_all_cookies()#print( browser.manage().getCookies() )
-
-
-
-
-
-
-#browser.quit()
